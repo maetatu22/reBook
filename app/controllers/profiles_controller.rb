0400f74850a17b0be81d7243b7a479416039fbdd
11nekomamire <nekomamiremy@gmail.com>
2020-11-06 00:23:20 +0900

    マイページ 未完成 仮commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
+
+  def index
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'profiles/new'
-  get 'profiles/create'
-  get 'profiles/edit'
-  get 'profiles/update'
   devise_for :users
-  # get 'post/index'
   root to: "posts#index"
 
   resources :posts do
