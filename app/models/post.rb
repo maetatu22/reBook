42bd2b7c5cc4ba7e7de0671949539fdff868e66c
11nekomamire <nekomamiremy@gmail.com>
2020-11-06 17:42:18 +0900

    プロフィール登録

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
+  enum purpose: { review: 0, output: 1 }
 
   with_options presence: true do
     validates :title
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
+  enum gender: { man: 0, woman: 1, other: 2}
 
   belongs_to :user
 end
