84577f2efeb044ac3e8a426c90324e8336d40b01
omotiyasan <sb.uv07ys@gmail.com>
2020-11-06 16:31:29 +0900

    コード省略

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:destroy, :edit, :show]
 
   def index
     @posts = Post.all
@@ -20,13 +21,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
@@ -37,12 +36,15 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :author, :genre_id, :content, :purpose).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
